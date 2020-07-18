3dae4a869f78211abb5c9e0c84a325ce00d5545f
Marco <marco.boutchelski@gmail.com>
2020-07-18 22:59:21 +0100

    Initial

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+
+    public static BuildManager instance;
+    private GameObject turretToBuild;
+    public GameObject stadardTurretPrefab;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    
+    private void Start()
+    {
+        turretToBuild = stadardTurretPrefab;
+    }
+
+
+    public GameObject getTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    private Transform target;
+    public float speed = 70f;
+    public GameObject impactEffect;
+
+    public void SetTarget(Transform _target)
+    {
+        target = _target;
+    }
+    void Update()
+    {
+        if (target == null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Vector3 dir = target.position - transform.position;
+        float distanceThisFrame = speed * Time.deltaTime;
+
+        if (dir.magnitude <= distanceThisFrame) // If target has hit
+        {
+            HitTarget();
+            return;
+        }
+        transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+    }
+
+    private void HitTarget()
+    {
+        GameObject effect = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(effect, 2f);
+        WaveSpawner.activeEnemies.Remove(target.gameObject);
+        Destroy(target.gameObject);
+        Destroy(gameObject);
+        
+    }
+}
