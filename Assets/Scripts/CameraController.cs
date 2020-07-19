3e06da8f534609e47f994482a2d5c9a3f3ccf603
Marco <marco.boutchelski@gmail.com>
2020-07-19 01:28:01 +0100

    Camera control forward added

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraController : MonoBehaviour
+{
+
+    public float panSpeed = 30f;
+
+    void Update()
+    {
+        if (Input.GetKey("w") || )
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+    }
+}
